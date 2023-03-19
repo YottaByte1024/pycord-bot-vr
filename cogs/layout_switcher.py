ef6a8e9ff88cb3b09c7a72512468b79c3c78e405
YottaByte1024 <52203300+YottaByte1024@users.noreply.github.com>
2023-03-19 17:36:47 +0300

    fix layout switcher

--- cogs/layout_switcher.py
+++ cogs/layout_switcher.py
@@ -4,19 +4,31 @@ from discord.ext import commands
 from discord.commands import Option
 
 
-layout = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`йцукенгшщзхъфывапролджэячсмитьбю.ё"
-                           'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'),
-                           "йцукенгшщзхъфывапролджэячсмитьбю.ёqwertyuiop[]asdfghjkl;'zxcvbnm,./`"
-                           'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,ЁQWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'))
+layout_en2ru = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
+                                 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'),
+                                 "йцукенгшщзхъфывапролджэячсмитьбю.ё"
+                                 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'))
+
+layout_ru2en = dict(zip(map(ord, "йцукенгшщзхъфывапролджэячсмитьбю.ё"
+                                 'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'),
+                                 "qwertyuiop[]asdfghjkl;'zxcvbnm,./`"
+                                 'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'))
 
 
 class LayoutSwitcher(BaseCog):
-    
-    @commands.message_command(name="Switch layout",)
-    async def message_layout_switcher(self, ctx: discord.ApplicationContext, message: discord.Message):
-        
-        result = message.content.translate(layout)
-        
+
+    @commands.message_command(name="Switch RU to EN",)
+    async def message_layout_switcher_ru2en(self, ctx: discord.ApplicationContext, message: discord.Message):
+
+        result = message.content.translate(layout_ru2en)
+
+        await ctx.respond(f"{message.author.mention} написал:\n {result}", ephemeral=False)
+
+    @commands.message_command(name="Switch EN to RU",)
+    async def message_layout_switcher_en2ru(self, ctx: discord.ApplicationContext, message: discord.Message):
+
+        result = message.content.translate(layout_en2ru)
+
         await ctx.respond(f"{message.author.mention} написал:\n {result}", ephemeral=False)
 
 
